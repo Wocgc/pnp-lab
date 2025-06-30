873c178b0a187c9e0cd4548dbaa92a3c3c1dbb2c
Wocgc <choigyucheol99@gmail.com>
2025-06-30 19:29:40 +0900

    ci/cd

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "cgc-mgmt-server" {
   instance_type               = var.mgmt_instance_type
   subnet_id                   = aws_subnet.private_subnet_a.id
   key_name                    = "cgckey"
-  associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.cgc-mgmt-sg.id]
 
   tags = {
