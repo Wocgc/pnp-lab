3b170a47bcd53b7ea3cc197163c284b07d0f60c3
Wocgc <choigyucheol99@gmail.com>
2025-06-30 15:43:22 +0900

    add: backend

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "cgclab-terraform-state"         # 네가 만든 S3 버킷 이름
+    key            = "terraform_eks/terraform.tfstate" # 저장될 경로
+    region         = "ap-northeast-2"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"                # Locking 테이블 
+  }
+}
+
