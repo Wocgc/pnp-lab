3bfd8809e24e4ae92f18eaa0b72df4eb37c8ef09
Wocgc <choigyucheol99@gmail.com>
2025-06-30 16:27:47 +0900

    ci/cd

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "cgc-test-terraform-state"          # 네가 만든 S3 버킷 이름
-    key            = "terraform_eks/terraform.tfstate" # 저장될 경로
-    region         = "ap-northeast-2"
-    encrypt        = true
+    bucket  = "cgc-test-terraform-state"        # 네가 만든 S3 버킷 이름
+    key     = "terraform_eks/terraform.tfstate" # 저장될 경로
+    region  = "ap-northeast-2"
+    encrypt = true
   }
 }
 
