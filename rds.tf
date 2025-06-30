e5bd519dc10110dd51f43143171b452e5baf1dc6
Wocgc <choigyucheol99@gmail.com>
2025-06-30 15:17:20 +0900

    fixed: ci/cd

---
+++ rds.tf
@@ -0,0 +1,23 @@
+resource "aws_db_subnet_group" "rds-snt-group" {
+  name       = "rds-snt-group"
+  subnet_ids = [aws_subnet.private_rds_a.id, aws_subnet.private_rds_c.id]
+
+  tags = {
+    Name = "rds-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "rds" {
+  identifier             = var.rds_identifier
+  engine                 = var.rds_engine
+  engine_version         = var.rds_engine_version
+  instance_class         = var.rds_instance_class
+  allocated_storage      = var.rds_allocated_storage
+  username               = var.rds_username
+  password               = var.rds_password
+  db_subnet_group_name   = aws_db_subnet_group.rds-snt-group.name
+  vpc_security_group_ids = [aws_security_group.db-sg.id]
+  multi_az               = var.rds_multi_az
+  publicly_accessible    = var.rds_publicly_accessible
+  skip_final_snapshot    = true
+}
